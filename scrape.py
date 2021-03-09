87d70f87fa1132f1447404d7cfa3d001e563d998
Josh Buentiempo <joshbuentiempo@MacBook-Pro.local>
2021-03-09 06:47:52 -0500

    commit1

--- scrape.py
+++ scrape.py
@@ -11,13 +11,18 @@ soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="bracket")
 #print(results.prettify())
 
-teams = results.find_all("div", class_="")
-for team in teams:
-    games.append(team)
-losers = games[1::2]
+def get_losers():
+    teams = results.find_all("div", class_="")
+    for team in teams:
+        games.append(team)
+    losers = games[1::2]
+    return losers
 #for i in losers:
 #    print(i, end='\n'*2)
 
-winners = results.find_all("div", class_="winner")
-for i in winners:
-    print(i, end='\n'*2)
+def get_winners():
+    winners = []
+    teams = results.find_all("div", class_="winner")
+    for i in teams:
+        winners.append(i)
+    return winners
