6085e0f9cbb660d1e527044ba708710f7f9d386c
joshuabuentiempo <joshbuentiempo@hotmail.com>
2022-09-01 03:49:25 -0400

    simplified log reg query

--- graph_data.py
+++ graph_data.py
@@ -1,12 +1,14 @@
+from re import A
 import numpy as np
 import matplotlib.pyplot as plt
+import pull_data2
 import pull_data
 
 var = ["fg", "fga", "fg_pct", "fg2", "fg2a", "fg2_pct", "fg3", "fg3a", "fg3_pct", "ft", "fta", "ft_pct", "oreb", "dreb", "treb", "ast", "stl", "blk", "tov", "pf", "pts"]
 
 
 def graph_log_reg(var1, train):
-    a = pull_data.difference(var1, train)
+    a = pull_data2.difference(var1, train)
     dif = []
     outcome = []
 
@@ -16,9 +18,6 @@ def graph_log_reg(var1, train):
     
     n_dif = np.array(dif)
     n_outcome = np.array(outcome)
-
-    print(n_dif)
-    print(n_outcome)
 
     plt.scatter(n_dif, n_outcome)
     plt.xlabel(var1)
@@ -53,8 +52,8 @@ def send_outcome(var1, train):
 
 #def graph_lda(stat1, stat2, train):
 
-stat1 = "fg"
-stat2 = "fga"
+stat1 = "pf"
+stat2 = "tov"
 train = 1
 
 a = pull_data.get_stats(stat1, stat2, train)
@@ -77,13 +76,11 @@ n_diff2_w = np.array(diff2_w)
 n_diff1_l = np.array(diff1_l)
 n_diff2_l = np.array(diff2_l)
 
-print(len(n_diff1_w))
-print(len(n_diff2_w))
-print(len(n_diff1_l))
-print(len(n_diff2_l))
 
 plt.scatter(n_diff1_w, n_diff2_w)
 plt.scatter(n_diff1_l, n_diff2_l)
 plt.xlabel(stat1)
 plt.ylabel(stat2)
 plt.show()
+
+#winner is blue
--- pull_data.py
+++ pull_data.py
@@ -7,55 +7,7 @@ mydb = mysql.connector.connect (
     database = "my_db"
 )
 
-
 mycursor = mydb.cursor()
-"""
-def difference(stat, train):
-    if train == 1:
-        sql = "SELECT * FROM games WHERE year < 2020"
-    else:
-        sql = "SELECT * FROM games WHERE year > 2020"
-    #sql = "SELECT Teamid, team, year, ft, pts FROM teams WHERE team = 'VCU' OR team = 'Duke'"
-    mycursor.execute(sql)
-
-    result = mycursor.fetchall()
-    result_a = []
-    result_b = []
-
-    for i in result:
-        sql_a = "SELECT " + stat +  " FROM teams WHERE team = %s AND year = %s"
-        val_a = (i[2], i[1])
-        mycursor.execute(sql_a, val_a)
-        result_a.append(mycursor.fetchall()[0][0])
-
-        sql_b = "SELECT " + stat +  " FROM teams WHERE team = %s AND year = %s"
-        val_b = (i[3], i[1])
-        mycursor.execute(sql_b, val_b)
-        result_b.append(mycursor.fetchall()[0][0])
-
-
-    t = 0
-    diff = []
-    win = []
-    while t < len(result_a):
-        diff.append(result_a[t] - result_b[t])
-        if result[t][6] == result[t][2]:
-            win.append(1)
-        else:
-            win.append(0)
-        t += 1
-
-    final = []
-    t2 = 0
-    while t2 < len(win):
-        final.append((diff[t2], win[t2]))
-        t2 += 1
-
-    return final
-
-
-#####################################################################
-"""
 
 def get_stats(stat1, stat2, train):
     if train == 1:
@@ -99,3 +51,4 @@ def get_stats(stat1, stat2, train):
         t2 += 1
 
     return final
+
